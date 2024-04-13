470f5ae36d174f0ee67f9bcfbb60ebf1d1e560d4
waiyanhtet1 <waiyanhtet1661390@gmail.com>
2024-04-13 14:15:07 +0630

    add: singlePage detail section ui

---
+++ src/components/slider/Slider.jsx
@@ -0,0 +1,7 @@
+import "./slider.scss";
+
+const Slider = () => {
+  return <div>slider</div>;
+};
+
+export default Slider;
--- src/pages/singlePage/SinglePage.jsx
+++ src/pages/singlePage/SinglePage.jsx
@@ -1,7 +1,35 @@
-import React from "react";
+import "./singlePage.scss";
+import Slider from "../../components/slider/Slider";
+import { singlePostData as data, userData } from "../../lib/dummyData";
 
 const SinglePage = () => {
-  return <div>single</div>;
+  return (
+    <div className="singlePage">
+      <div className="details">
+        <div className="wrapper">
+          <Slider />
+          <div className="top">
+            <div className="info">
+              <h1 className="info-title">{data.title}</h1>
+              <div className="info-address">
+                <img src="/pin.png" alt="" />
+                <span>{data.address}</span>
+              </div>
+              <div className="info-price">$ {data.price}</div>
+            </div>
+            <div className="user">
+              <img src={userData.img} alt="" />
+              <h4>{userData.name}</h4>
+            </div>
+          </div>
+          <div className="bottom">{data.description}</div>
+        </div>
+      </div>
+      <div className="features">
+        <div className="wrapper">features</div>
+      </div>
+    </div>
+  );
 };
 
 export default SinglePage;
