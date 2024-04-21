f15f8e3a7707448796156aee450e9bd8ae54be09
waiyanhtet1 <waiyanhtet1661390@gmail.com>
2024-04-21 16:04:15 +0630

    api change

--- src/lib/apiRequest.js
+++ src/lib/apiRequest.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const apiRequest = axios.create({
   // baseURL: `${config.SERVER_URI}/api`,
-  baseURL: `https://real-estate-backend-8xxt.onrender.com`,
+  baseURL: `https://real-estate-backend-8xxt.onrender.com/api`,
   withCredentials: true,
 });
 
