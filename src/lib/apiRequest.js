ab1bf03d7aa3d3e71bb60249445b26daa9f59056
waiyanhtet1 <waiyanhtet1661390@gmail.com>
2024-04-21 15:19:59 +0630

    refactor: api route change

--- src/lib/apiRequest.js
+++ src/lib/apiRequest.js
@@ -1,9 +1,8 @@
 import axios from "axios";
-import config from "../config";
 
 const apiRequest = axios.create({
-  baseURL: `${config.SERVER_URI}/api`,
-  // baseURL: `https://real-estate-backend-vwr6.onrender.com/api`,
+  // baseURL: `${config.SERVER_URI}/api`,
+  baseURL: `https://real-estate-backend-vwr6.onrender.com/api`,
   withCredentials: true,
 });
 
